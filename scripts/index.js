82978f451d5de37c4479e0ec09b21f623920e395
zazc <88509080+zazcdev@users.noreply.github.com>
2022-04-10 11:07:26 -0300

    updating contact page to clear input fields on submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,47 @@
 
 //Displays the mobile navigation menu when the hamburger menu is clicked
 function trigger() {
-  const nav = document.getElementById("primary-nav");
-  const icon = document.getElementById("icon");
-  const logo = document.getElementById("logo");
+  const nav = document.getElementById('primary-nav');
+  const icon = document.getElementById('icon');
+  const logo = document.getElementById('logo');
 
-  if (nav.className === "nav-container") {
-    nav.className += " responsive";
-    icon.innerHTML = "close";
-    logo.style.display = "none";
+  if (nav.className === 'nav-container') {
+    nav.className += ' responsive';
+    icon.innerHTML = 'close';
+    logo.style.display = 'none';
   } else {
-    nav.className = "nav-container";
-    icon.innerHTML = "menu";
-    logo.style.display = "flex";
+    nav.className = 'nav-container';
+    icon.innerHTML = 'menu';
+    logo.style.display = 'flex';
   }
 }
 
-
-
-const links = document.querySelector("body");
+//creates smooth-scrolling effect 
+const links = document.querySelector('body');
 
 for (const link of links) {
-  link.addEventListener("click", clickHandler);
+  link.addEventListener('click', clickHandler);
 }
 
 function clickHandler(e) {
   e.preventDefault();
-  const href = this.getAttribute("href");
+  const href = this.getAttribute('href');
 
   document.querySelector(href).scrollIntoView({
-    behavior: "smooth"
+    behavior: 'smooth'
   });
 }
 
+const btn = document.getElementById('contact-btn');
+
+btn.addEventListener('click', function handleClick(event) {
+
+  event.preventDefault();
+
+  const emailInput = document.getElementById('email');
+  const messageInput = document.getElementById('message');
+
+  // 👇️ clear input field
+  emailInput.value = '';
+  messageInput.value = '';
+});
